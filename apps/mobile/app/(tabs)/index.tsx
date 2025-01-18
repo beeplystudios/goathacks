fb248982e6dfe9fa8f6c2aa866d410432f8457d5
SantiagoVira <santiagovira06@gmail.com>
2025-01-18 08:45:17 -0500

    stuff

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import "../../global.css"
+import "../../global.css";
 import { StyleSheet, Text } from "react-native";
 
 import ThemedText from "@/components/ThemedText";
@@ -9,49 +9,11 @@ export default function HomeScreen() {
   const [data] = trpc.rat.useSuspenseQuery();
 
   return (
-    <ThemedView style={styles.content}>
-      <ThemedView style={styles.titleContainer}>
+    <ThemedView className="flex-1 p-8 gap-4 overflow-hidden pt-20">
+      <ThemedView className="flex-row items-center gap-2">
         <ThemedText type="title">Projects</ThemedText>
         <Text className="text-white">{data}</Text>
       </ThemedView>
     </ThemedView>
   );
 }
-
-const styles = StyleSheet.create({
-  content: {
-    flex: 1,
-    padding: 32,
-    gap: 16,
-    overflow: "hidden",
-    paddingTop: 86,
-  },
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  noProjectsWrapper: {
-    alignItems: "center",
-    justifyContent: "center",
-    flex: 1,
-    width: "100%",
-    padding: "10%",
-    marginBottom: 86,
-    gap: 8,
-  },
-  noProjectsText: {
-    fontSize: 32,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  grid: {
-    marginBottom: 52,
-  },
-  footer: {
-    position: "absolute",
-    right: 12,
-  },
-});
