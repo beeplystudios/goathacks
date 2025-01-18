b77bce76d65afe6976be17cd0a751ff1aaff6c9b
Nirjhor Nath <nirjhor.nath@gmail.com>
2025-01-18 13:49:26 -0500

    stop drizzle error

--- packages/trpc/src/__root.ts
+++ packages/trpc/src/__root.ts
@@ -1,12 +1,9 @@
-import { test } from "@goathacks/db/schema";
 import { publicProcedure, router } from "./trpc";
 
 export const appRouter = router({
     rat: publicProcedure.query(async ({ctx}) => {
         console.log("hi there");
-        const query = await ctx.db.select().from(test).all();
-
-        return query[0].id;
+        return "test"
     })
 });
  
