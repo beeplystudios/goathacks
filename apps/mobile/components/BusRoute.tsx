eb81a9c1ee051164ec1005e07b78b4a15b19ee65
SantiagoVira <santiagovira06@gmail.com>
2025-01-19 03:29:21 -0500

    add bus route list

--- apps/mobile/components/BusRoute.tsx
+++ apps/mobile/components/BusRoute.tsx
@@ -3,12 +3,35 @@ import { BusRouteType } from "../constants/BusData";
 
 const BusRoute: React.FC<BusRouteType> = ({ route, routeColor }) => {
   return (
-    <View className="w-full p-12">
-      <Text
-        className="px-8 py-4 rounded"
-        style={{ backgroundColor: routeColor }}>
-        {route}
-      </Text>
+    <View
+      style={{
+        padding: 12,
+        borderColor: "rgba(255,255,255,0.2)",
+        borderBottomWidth: 1,
+        width: "100%",
+        flexDirection: "row",
+      }}>
+      <View
+        style={{ flex: 2, flexDirection: "row", justifyContent: "flex-end" }}>
+        <View
+          style={{
+            backgroundColor: routeColor,
+            paddingHorizontal: 8,
+            paddingVertical: 4,
+            borderRadius: 8,
+          }}>
+          <Text>{route}</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          flex: 7,
+          flexDirection: "row",
+          alignItems: "center",
+          paddingLeft: 10,
+        }}>
+        <Text style={{ color: "white", fontWeight: "medium" }}>All good!</Text>
+      </View>
     </View>
   );
 };
